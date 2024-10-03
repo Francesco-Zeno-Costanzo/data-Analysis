a30e6a6805b93c3d06f81edef88f4f8127f98a61
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2024-10-03 22:37:37 +0200

    new version of the code, more general

--- errori.py
+++ errori.py
@@ -1,108 +1,79 @@
+"""
+Code to calculate errors on a generic function
+"""
 import numpy as np
 import sympy as sp
-'''
-Il codice è povero di commenti perché è abbastanza autoesplicativo, si precisa però che:
-I) sp.Symbol  difinisce i simboli da utilizzare;
-II) sp.diff fa la derivata di una funzione (primo argomento) rispetto ad una variabile (secondo argomento);
-III) .subs sostituisce al simbolo (primo argomento) il valore numerico (secondo argomento).
-'''
-##somma
 
-def summa(x, dx, y, dy):
-    f=x+y
-    df=np.sqrt(dx**2+dy**2)
-    return f, df
+def compute_error(f, variables, data, error):
+    '''
+    Function to propagate the error of an arbritay function f.
+    For example if f is f(x, y, z) the error, df, will be:
 
-##sottrazione
-def diff(x, dx, y, dy):
-    f=x-y
-    df=np.sqrt(dx**2+dy**2)
-    return f, df
+    df = sqrt( (df(x,y,z)/dx * dx)**2 + (df(x,y,z)/dx * dx)**2 + (df(x,y,z)/dx * dx)**2 )
 
-##prodotto
+    Parameters
+    ----------
+    f : sympy.core.(something)
+        function, written with sympy simbols,
+        of which to compute the error
+    variables : list
+        list of variables of f, each element
+        mu be a sympy.core.symbol.Symbol
+    data : list
+        list of the numerical value of the variables
+    error : list
+        list of the error on data
+    
+    Returns
+    -------
+    central_value : float
+        value of f comuted on data
+    error : float
+        error on central value
+    
+    Example
+    -------
+    >>> x = sp.Symbol('x')
+    >>> y = sp.Symbol('y')
+    >>> X = [x, y]
+    >>> f = ((1/x) + (1/y))**(-1)
+    >>> compute_error(f, X, [1.3, 2], [0.1, 0.1])
+    >>> (0.7499999999999999, 0.044393977016173036)
+    '''
 
-def prod(x, dx, y, dy):
-    f=x*y
-    df=np.sqrt((dx/x)**2+(dy/y)**2)*f
-    return f, df
+    # Compute the central value i.e. f(data)
+    # We use func beacuse we sobsistute on variable at time
+    func = f 
+    for var, val in zip(variables, data):
+        func = func.subs(var, val)
+    
+    central_value = float(func)
 
-##divisione
+    # Compute the error on f
+    # First of all we compute al derivatives of f respect each variable
+    d_func = []
+    for var in variables:
+        d_func.append(sp.diff(f, var))
+    
+    # Secondly we compute the numerical value of all the derivatives
+    d_func_v = []
+    for deriv in d_func:
+        for var, val in zip(variables, data):
+            deriv = deriv.subs(var, val)
+        d_func_v.append(deriv)
+    
+    # Finally we compute the error adding the squares of derivative and error
+    d_func_v_2 = [(df*err)**2 for df, err in zip(d_func_v, error)]
+    error      = np.sqrt(float(sum(d_func_v_2)))
 
-def rapp(x, dx, y, dy):
-    f=x/y
-    df=np.sqrt((dx/x)**2+(dy/y)**2)*f
-    return f, df
+    return central_value, error
 
-##parallelo
-x = sp.Symbol('x')
-y = sp.Symbol('y')
+if __name__ == "__main__":
 
-def par(x1, dx1, y1, dy1):
-    f1=((1/x)+(1/y))**(-1)
-    f=f1.subs(x,x1).subs(y,y1)
+    x = sp.Symbol('x')
+    y = sp.Symbol('y')
 
-    a=sp.diff(f1, x).subs(x,x1).subs(y,y1)
-    b=sp.diff(f1, y).subs(x,x1).subs(y,y1)
-    df1=((a*dx1)**2 + (b*dy1)**2)
-    df=np.sqrt(float(df1))
-    return f, df
+    X = [x, y]
+    f = ((1/x) + (1/y))**(-1)
 
-##boh ma capita spesso
-x = sp.Symbol('x')
-y = sp.Symbol('y')
-z = sp.Symbol('z')
-t = sp.Symbol('t')
-
-def B(x1, dx1, y1, dy1, z1, dz1, t1, dt1):
-    f1=((x-y)/(z+t))
-    f=f1.subs(x,x1).subs(y,y1).subs(z,z1).subs(t,t1)
-
-    a=sp.diff(f1, x).subs(x,x1).subs(y,y1).subs(z,z1).subs(t,t1)
-    b=sp.diff(f1, y).subs(x,x1).subs(y,y1).subs(z,z1).subs(t,t1)
-    c=sp.diff(f1, z).subs(x,x1).subs(y,y1).subs(z,z1).subs(t,t1)
-    d=sp.diff(f1, t).subs(x,x1).subs(y,y1).subs(z,z1).subs(t,t1)
-
-    df1=((a*dx1)**2 + (b*dy1)**2+ (c*dz1)**2 + (d*dt1)**2 )
-    df=np.sqrt(float(df1))
-    return f, df
-
-
-## f di taglio dal fit
-x = sp.Symbol('x')
-y = sp.Symbol('y')
-z = sp.Symbol('z')
-t = sp.Symbol('t')
-
-def H(x1, dx1, y1, dy1, z1, dz1, t1, dt1):
-
-    f1=10**(((x-y)/(z-t))/20)
-    f=f1.subs(x,x1).subs(y,y1).subs(z,z1).subs(t,t1)
-
-    a=sp.diff(f1, x).subs(x,x1).subs(y,y1).subs(z,z1).subs(t,t1)
-    b=sp.diff(f1, y).subs(x,x1).subs(y,y1).subs(z,z1).subs(t,t1)
-    c=sp.diff(f1, z).subs(x,x1).subs(y,y1).subs(z,z1).subs(t,t1)
-    d=sp.diff(f1, t).subs(x,x1).subs(y,y1).subs(z,z1).subs(t,t1)
-
-    df1=((a*dx1)**2 + (b*dy1)**2+ (c*dz1)**2 + (d*dt1)**2 )
-    df=np.sqrt(float(df1))
-    return f, df
-
-##
-x = sp.Symbol('x')
-y = sp.Symbol('y')
-z = sp.Symbol('z')
-t = sp.Symbol('t')
-
-def G(x1, dx1, y1, dy1, z1, dz1, t1, dt1):
-
-    f1=(x*y)*sp.log((1+(4.1/3.5)*(t/(t+z)))/(1-(t/(t+z))))
-    f=f1.subs(x,x1).subs(y,y1).subs(z,z1).subs(t,t1)
-
-    a=sp.diff(f1, x).subs(x,x1).subs(y,y1).subs(z,z1).subs(t,t1)
-    b=sp.diff(f1, y).subs(x,x1).subs(y,y1).subs(z,z1).subs(t,t1)
-    c=sp.diff(f1, z).subs(x,x1).subs(y,y1).subs(z,z1).subs(t,t1)
-    d=sp.diff(f1, t).subs(x,x1).subs(y,y1).subs(z,z1).subs(t,t1)
-
-    df1=((a*dx1)**2 + (b*dy1)**2+ (c*dz1)**2 + (d*dt1)**2 )
-    df=np.sqrt(float(df1))
-    return f, df
+    print(compute_error(f, X, [1.2, 2], [0.1, 0.15]))
